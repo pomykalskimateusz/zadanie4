0be6e8389133805731153bdb784b8f171f8a03bd
mpomykalski <pomykalskimateusz@gmail.com>
2020-06-21 13:16:36 +0200

    remove rest template from bean config

--- src/main/java/rekrutacja/zad4/BeanConfiguration.java
+++ src/main/java/rekrutacja/zad4/BeanConfiguration.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfiguration
@@ -13,11 +12,5 @@ public class BeanConfiguration
     public ObjectMapper objectMapper()
     {
         return new ObjectMapper();
-    }
-
-    @Bean
-    public RestTemplate restTemplate()
-    {
-        return new RestTemplate();
     }
 }
